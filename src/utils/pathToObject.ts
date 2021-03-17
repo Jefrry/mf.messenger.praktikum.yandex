4028d06e8466f51c3e19770532eed1cf48d70c94
Егор Ильин <skeetch2010@gmail.com>
2021-03-17 18:42:21 +0400

    Добавил тесты

--- src/utils/pathToObject.ts
+++ src/utils/pathToObject.ts
@@ -1,5 +1,6 @@
-function pathToObject (obj: { [key:string]: any }, path: string, defaultValue?: any): any {
+function pathToObject(obj: { [key: string]: any }, path: string, defaultValue?: any): any {
     const keys = path.split('.');
+    if (keys.length < 1 || path === '') throw Error('Путь должен быть строкой и содержать символ "." разделения');
 
     let result = obj;
     for (let key of keys) {
@@ -12,4 +13,4 @@ function pathToObject (obj: { [key:string]: any }, path: string, defaultValue?: 
 
     return result ?? defaultValue;
 };
-export {pathToObject}
+export { pathToObject }
