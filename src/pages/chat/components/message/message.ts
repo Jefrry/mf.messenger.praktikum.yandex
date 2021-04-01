55c501e2c89d45492aed64d0ac9c15cd301e4be6
Егор Ильин <skeetch2010@gmail.com>
2021-04-01 13:51:03 +0400

    Правка

--- src/pages/chat/components/message/message.ts
+++ src/pages/chat/components/message/message.ts
@@ -3,12 +3,11 @@ import {Templator} from "../../../../modules/templator.js";
 import {template} from './messsage.tmpl.js'
 import { IMessage } from '../../../../controllers/chats/index.js';
 import { IBlockCompProps } from '../../../../components/block/block.type.js';
+
+interface IMessageProps extends IMessage, IBlockCompProps { }
 class MessageComp extends Block {
     userId: number;
-    // @ts-ignore
-    // ЭТО ВООБЩЕ ЧТО?
-    // @ts-ignore
-    constructor(protected props: IMessage extends IBlockCompProps) {
+    constructor(protected props: IMessageProps) {
         super("div", props, { "class": `message relative pa-3 pr-14 ${props.class ?? ''}` });
         this.userId = props.user_id
     }
